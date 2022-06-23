9c400f04a2b88a03b5e34892c0bf3940f1813d0b
Josh White <jojawhi@gmail.com>
2022-06-23 00:24:48 -0700

    updated env.local with firebase credentials, reinstalled yarn and updated yarn.lock with eslint-prettier config, added some console.logs and temporarily commented out  some variables that were flagged by eslint

--- src/components/user/login.js
+++ src/components/user/login.js
@@ -1,37 +1,37 @@
-import { useAuth } from './auth';
-import { Form, FormGroup, Row, Col, Input, Label, Button } from 'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
-import { useEffect, useState } from 'react';
-import { StyledFirebaseAuth } from 'react-firebaseui';
+import { useAuth } from './auth'
+import { Form, FormGroup, Row, Col, Button } from 'reactstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons'
+import { useEffect, useState } from 'react'
+import { StyledFirebaseAuth } from 'react-firebaseui'
 
 const componentLoginFroms = {
-  'login': LoginForm,
-  'email': EmailLogin,
+  login: LoginForm,
+  email: EmailLogin,
 }
 
 export default function Login(props) {
-  const { user } = useAuth();
+  const { user } = useAuth()
 
-  const [form, setForm] = useState('login');
+  const [form, setForm] = useState('login')
 
-  const Component = componentLoginFroms[form];
+  const Component = componentLoginFroms[form]
 
   // if user exists, redirect to home
   useEffect(() => {
     if (user) {
-      const returnTo = props.location.state?.appState?.returnTo || '/';
+      const returnTo = props.location.state.appState.returnTo || '/'
 
-      props.history.replace(returnTo);
+      props.history.replace(returnTo)
     }
-  }, [user, props.history]);
+  }, [user, props.history])
 
   const retVal = (
     /* todo - wrap in layout container */
     <div className="container-lg container-fluid mt-lg-5">
       <div className="row pt-lg-5">
         <div className="col-md-3 offset-md-2">
-          <h3 className='text-primary'>Log in or create an account</h3>
+          <h3 className="text-primary">Log in or create an account</h3>
           <p>Quickly get started by signing in using your existing accounts.</p>
         </div>
         <div className="col-md-6">
@@ -44,70 +44,78 @@ export default function Login(props) {
         }
       `}</style> */}
     </div>
-  );
+  )
 
-  return retVal;
+  return retVal
 }
 
 function LoginForm(props) {
-  const { firebase, setForm } = props;
+  const { firebase, setForm } = props
 
   // right now, the oauth form shows a firebae domain.
   // do not worory, others use magic link as well https://stackoverflow.com/questions/47532134/changing-the-domain-shown-by-google-account-chooser
 
   const handleLogin = (provider) => {
-    return firebase.auth().signInWithPopup(provider)
+    return firebase
+      .auth()
+      .signInWithPopup(provider)
       .then((result) => {
-        var credential = result.credential;
+        // var credential = result.credential
 
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
+        // // This gives you a Google Access Token. You can use it to access the Google API.
+        // var token = credential.accessToken
+        // // The signed-in user info.
+        // var user = result.user
         // ...
-      }).catch((error) => {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        console.error(error);
-        alert(error);
+        console.log(result)
+      })
+      .catch((error) => {
+        // // Handle Errors here.
+        // var errorCode = error.code
+        // var errorMessage = error.message
+        // // The email of the user's account used.
+        // var email = error.email
+        // // The firebase.auth.AuthCredential type that was used.
+        // var credential = error.credential
+        console.error(error)
+        alert(error)
         // ...
-      });
+      })
   }
 
   const handleGoogleLogin = (e) => {
-    e.preventDefault();
-    const googleProvider = new firebase.auth.GoogleAuthProvider();
-    return handleLogin(googleProvider);
+    e.preventDefault()
+    const googleProvider = new firebase.auth.GoogleAuthProvider()
+    return handleLogin(googleProvider)
   }
 
   const handleFacebookLogin = (e) => {
-    e.preventDefault();
-    const facebookProvider = new firebase.auth.FacebookAuthProvider();
-    return handleLogin(facebookProvider);
+    e.preventDefault()
+    const facebookProvider = new firebase.auth.FacebookAuthProvider()
+    return handleLogin(facebookProvider)
   }
 
   const handleEmailLogin = (e) => {
-    e.preventDefault();
-    setForm('email');
+    e.preventDefault()
+    setForm('email')
   }
 
   const terms = (
-    <a href='#' target='_blank'>Terms of Service</a>
-  );
+    <a href="#" target="_blank">
+      Terms of Service
+    </a>
+  )
 
   const privacy = (
-    <a href='#' target='_blank'>Privacy Policy</a>
-  );
+    <a href="#" target="_blank">
+      Privacy Policy
+    </a>
+  )
 
   const retVal = (
     <Form>
       <FormGroup>
-        <p className='small text-left text-muted font-weight-light'>
+        <p className="small text-left text-muted font-weight-light">
           By proceeding, you are agreeing to the {terms} and {privacy}.
         </p>
       </FormGroup>
@@ -116,8 +124,11 @@ function LoginForm(props) {
           <FormGroup>
             {/*   <Label for="about.firstName">First Name</Label>
             <Input type="text" /> */}
-            <Button className='btn-block btn-light d-flex flex-row justify-content-around align-items-center' onClick={handleGoogleLogin}>
-              <FontAwesomeIcon icon={faGoogle} className='mr-lg-1' />
+            <Button
+              className="btn-block btn-light d-flex flex-row justify-content-around align-items-center"
+              onClick={handleGoogleLogin}
+            >
+              <FontAwesomeIcon icon={faGoogle} className="mr-lg-1" />
               Continue with Google
             </Button>
           </FormGroup>
@@ -125,41 +136,45 @@ function LoginForm(props) {
         <Col md={6}>
           <FormGroup>
             {/* for some reason btn-primary does not work? */}
-            <Button className='btn-block d-flex flex-row justify-content-around align-items-center' color='primary' onClick={handleFacebookLogin}>
+            <Button
+              className="btn-block d-flex flex-row justify-content-around align-items-center"
+              color="primary"
+              onClick={handleFacebookLogin}
+            >
               <FontAwesomeIcon icon={faFacebook} />
               Continue with Facebook
             </Button>
           </FormGroup>
         </Col>
       </Row>
-      <p className='small text-center font-weight-light'>
-        or
-      </p>
+      <p className="small text-center font-weight-light">or</p>
       <FormGroup>
-      <p className='small text-center text-muted font-weight-light'>
-      <a href='#' onClick={handleEmailLogin}>Login with email address.</a>
+        <p className="small text-center text-muted font-weight-light">
+          <a href="#" onClick={handleEmailLogin}>
+            Login with email address.
+          </a>
         </p>
       </FormGroup>
     </Form>
-  );
+  )
 
-  return retVal;
+  return retVal
 }
 
 function EmailLogin(props) {
-  const { firebase } = props;
+  const { firebase } = props
 
   // Configure FirebaseUI.
   const uiConfig = {
     // Popup signin flow rather than redirect flow.
     signInFlow: 'popup',
     // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: props.location.state?.appState.returnTo || '/',
+    signInSuccessUrl: props.location.state.appState.returnTo || '/',
     // We will display Google and Facebook as auth providers.
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    ],
-  };
+    signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+  }
 
-  return <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+  return (
+    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+  )
 }
