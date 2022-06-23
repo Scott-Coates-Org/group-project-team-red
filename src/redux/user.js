bfdf0a053d00dfde8786a428e8463b4e8038009c
jojawhi <82842406+jojawhi@users.noreply.github.com>
2022-06-23 07:20:38 -0700

    josh-initial-setup (#35)
    * updated env.local with firebase credentials, reinstalled yarn and updated yarn.lock with eslint-prettier config, added some console.logs and temporarily commented out
     some variables that were flagged by eslint
    
    * Delete yarn.lock
    
    yarn lock and package-lock.json are generated after installing dependencies, and alike node_modules should not be uploaded to github.
    
    * Update .gitignore
    
    yarn.lock added to gitignore
    
    Co-authored-by: Pablo Maffioli <92764736+pablo-maff@users.noreply.github.com>

--- src/redux/user.js
+++ src/redux/user.js
@@ -1,33 +1,32 @@
 // https://dev.to/thatgalnatalie/how-to-get-started-with-redux-toolkit-41e
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   data: {},
   isLoaded: false,
   hasErrors: false,
-};
+}
 
 const user = createSlice({
-  name: "user",
+  name: 'user',
   initialState,
   reducers: {
     getData: (state) => {
+      console.log(state)
     },
 
     getDataSuccess: (state, action) => {
-      state.isLoaded = true;
-      state.data = action.payload;
+      state.isLoaded = true
+      state.data = action.payload
     },
 
     getDataFailure: (state) => {
-      state.isLoaded = true;
-      state.hasErrors = true;
+      state.isLoaded = true
+      state.hasErrors = true
     },
-  }
-});
+  },
+})
 
-export const reducer = user.reducer;
+export const reducer = user.reducer
 
-export const {
-  getData, getDataSuccess, getDataFailure
-} = user.actions;
+export const { getData, getDataSuccess, getDataFailure } = user.actions
--- src/redux/widget.js
+++ src/redux/widget.js
@@ -1,126 +1,140 @@
 // https://dev.to/thatgalnatalie/how-to-get-started-with-redux-toolkit-41e
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import firebaseClient from 'firebase/client';
+import firebaseClient from 'firebase/client'
 
 const initialState = {
   data: {},
   isLoaded: false,
   hasErrors: false,
-};
+}
 
 const widget = createSlice({
-  name: "widget",
+  name: 'widget',
   initialState,
   reducers: {
     getData: (state) => {
+      console.log(state)
     },
 
     getDataSuccess: (state, action) => {
-      state.isLoaded = true;
-      state.data = action.payload;
+      state.isLoaded = true
+      state.data = action.payload
     },
 
     getDataFailure: (state, action) => {
-      state.isLoaded = true;
-      state.hasErrors = true;
+      state.isLoaded = true
+      state.hasErrors = true
+      console.log(action)
     },
 
     createDataFailure: (state) => {
-      state.hasErrors = true;
+      state.hasErrors = true
     },
-  }
-});
+  },
+})
 
-export const reducer = widget.reducer;
+export const reducer = widget.reducer
 
-export const {
-  getData, getDataSuccess, getDataFailure, createDataFailure
-} = widget.actions;
+export const { getData, getDataSuccess, getDataFailure, createDataFailure } =
+  widget.actions
 
 export const fetchAllWidgets = createAsyncThunk(
-  "widget/fetchAllWidgets",
+  'widget/fetchAllWidgets',
   async (_, thunkAPI) => {
     // Set the loading state to true
-    thunkAPI.dispatch(getData());
+    thunkAPI.dispatch(getData())
 
     try {
-      const data = await _fetchAllWidgetsFromDb();
-      thunkAPI.dispatch(getDataSuccess(data));
+      const data = await _fetchAllWidgetsFromDb()
+      thunkAPI.dispatch(getDataSuccess(data))
     } catch (error) {
       console.error('error', error)
       // Set any erros while trying to fetch
-      thunkAPI.dispatch(getDataFailure(error));
+      thunkAPI.dispatch(getDataFailure(error))
     }
   }
-);
+)
 
 export const createWidget = createAsyncThunk(
-  "widget/createWidget",
+  'widget/createWidget',
   async (payload, thunkAPI) => {
     try {
-      await _createWidget(payload.title, payload.type, payload.photo);
+      await _createWidget(payload.title, payload.type, payload.photo)
     } catch (error) {
       console.error('error', error)
       // Set any erros while trying to fetch
-      thunkAPI.dispatch(createDataFailure());
+      thunkAPI.dispatch(createDataFailure())
     }
   }
-);
+)
 
 export const savePhoto = createAsyncThunk(
-  "widget/savePhoto",
+  'widget/savePhoto',
   async (payload) => {
-    const file = payload.file;
+    const file = payload.file
 
     try {
-      const fileName = _appendToFilename(file.name, "_" + Date.now());
-      const uploadTask = _updloadFile(fileName, file);
+      const fileName = _appendToFilename(file.name, '_' + Date.now())
+      const uploadTask = _updloadFile(fileName, file)
 
       const uploadPromise = new Promise((resolve, reject) => {
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            console.log('progress:', progress)
+          },
+          (error) => {
+            reject(error)
+          },
+          () => {
+            uploadTask.snapshot.ref
+              .getDownloadURL()
+              .then((downloadURL) => resolve(downloadURL))
+              .catch(reject)
+          }
+        )
+      })
 
-        uploadTask.on('state_changed', snapshot => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log('progress:', progress);
+      const downloadURL = await uploadPromise
 
-        }, error => {
-          reject(error);
-        }, () => {
-          uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => resolve(downloadURL)).catch(reject);
-        });
-      });
-
-      const downloadURL = await uploadPromise;
-
-      return downloadURL;
+      return downloadURL
     } catch (error) {
-      alert('Error saving photo: ' + JSON.stringify(error));
+      alert('Error saving photo: ' + JSON.stringify(error))
     }
   }
-);
+)
 
 async function _fetchAllWidgetsFromDb() {
-  const snapshot = await firebaseClient.firestore().collection('widgets').get();
+  const snapshot = await firebaseClient.firestore().collection('widgets').get()
 
-  const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
 
-  return data;
+  return data
 }
 
 async function _createWidget(title, type, photo) {
-  const doc = await firebaseClient.firestore().collection('widgets').add({ title, type, photo });
+  const doc = await firebaseClient
+    .firestore()
+    .collection('widgets')
+    .add({ title, type, photo })
 
-  return doc;
+  return doc
 }
 
 // https://stackoverflow.com/a/31205878/173957
 function _appendToFilename(filename, string) {
-  var dotIndex = filename.lastIndexOf(".");
-  if (dotIndex == -1) return filename + string;
-  else return filename.substring(0, dotIndex) + string + filename.substring(dotIndex);
+  var dotIndex = filename.lastIndexOf('.')
+  if (dotIndex === -1) return filename + string
+  else
+    return (
+      filename.substring(0, dotIndex) + string + filename.substring(dotIndex)
+    )
 }
 
 function _updloadFile(fileName, file) {
-  const uploadTask = firebaseClient.storage().ref(`/${fileName}`).put(file);
+  const uploadTask = firebaseClient.storage().ref(`/${fileName}`).put(file)
 
-  return uploadTask;
+  return uploadTask
 }
