0d49e9ef50a2360dc64fc7c32db8481e2c19e7cd
Josh White <jojawhi@gmail.com>
2022-06-25 22:08:59 -0700

    initialized admin dashboard component; added dashboard component to protected route on app.js

---
+++ src/components/admin-ui/dashboard/dashboard.component.jsx
@@ -0,0 +1,11 @@
+import SidebarNav from '../sidebar-nav/sidebar-nav.component'
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <SidebarNav></SidebarNav>
+    </div>
+  )
+}
+
+export default Dashboard
