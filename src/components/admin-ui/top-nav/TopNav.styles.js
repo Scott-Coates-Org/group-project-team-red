c2adecf26dcdcb7a6f9ce00f48700e0736f1d5de
Josh White <jojawhi@gmail.com>
2022-07-03 09:11:43 -0700

    tweak topNav styles

--- src/components/admin-ui/top-nav/TopNav.styles.js
+++ src/components/admin-ui/top-nav/TopNav.styles.js
@@ -10,7 +10,7 @@ export const TopNavContainer = styled.div`
 export const LogoContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: left;
+  justify-content: center;
 `
 
 export const FrogLogo = styled.img`
