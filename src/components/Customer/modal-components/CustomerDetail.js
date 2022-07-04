43fe81b346193831fa9f3bc8e65bb2ec1ff9d005
JSdev-coder <66922979+JSdevS-coder@users.noreply.github.com>
2022-07-05 00:40:51 +0200

    adding stripe 50% not functional yet

---
+++ src/components/Customer/modal-components/CustomerDetail.js
@@ -0,0 +1,247 @@
+import { useState } from 'react'
+//style
+import { StyledModal } from '../styled/Modal.styles'
+import { StyledRange } from '../styled/Range.styles'
+import { StyledFlexColumn } from '../styled/FlexColumn.styles'
+import { StyledFlexRow } from '../styled/FlexRow.styles'
+import { StyledButton } from '../styled/Button.styles'
+import { Link } from 'react-router-dom'
+//assets
+import { FaArrowLeft } from 'react-icons/fa'
+
+//components
+
+import Recipe from '../modals/Recipe'
+import PaymentDetails from './PaymentDetails'
+import { Form } from 'reactstrap'
+import { StyledInfoBlue } from '../styled/InfoBlue.styles'
+
+export default function CustomerDetails() {
+  const [step3, setStep3] = useState(true)
+  const [step4, setStep4] = useState(false)
+
+  const [email, setEmail] = useState('')
+  const [cellNumber, setCellNumber] = useState('')
+  const [zipCode, setZipCode] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+
+  const [newUser, setNewUser] = useState({})
+
+  const handleSubmit = () => {
+    setNewUser(firstName, lastName, email, cellNumber, zipCode)
+  }
+  console.log(newUser)
+  return (
+    <StyledFlexRow>
+      {step3 && (
+        <StyledModal
+          top={0}
+          left="0"
+          height="100%"
+          width="45%"
+          style={{ overflowY: 'scroll' }}
+        >
+          <div>
+            <hr />
+            <StyledRange
+              bg="#d9d9d9"
+              width="100%"
+              style={{ position: 'static', marginTop: '3em' }}
+            >
+              <StyledRange
+                bg="#35bd21"
+                width="80%"
+                style={{ position: 'static', marginTop: '3em' }}
+              />
+            </StyledRange>
+
+            <StyledFlexColumn
+              style={{
+                marginTop: '2em',
+              }}
+            >
+              <div>
+                <FaArrowLeft /> <span>Step 3 of 4</span>
+              </div>
+
+              <h3>Enter your details</h3>
+            </StyledFlexColumn>
+          </div>
+          <StyledInfoBlue>
+            Please review our Safety Video and Code of Conduct prior to visiting
+            the Park!
+            https://www.altitudetrampolinepark.com/locations/appleton/about/safety
+          </StyledInfoBlue>
+          <Form onSubmit={() => handleSubmit()}>
+            <StyledFlexColumn>
+              <StyledFlexRow padding="0.5em 0" justify="space-between">
+                <StyledFlexColumn>
+                  <label>
+                    <p>First name:</p>
+                    <input
+                      type="text"
+                      onChange={(e) => setFirstName(e.target.value)}
+                      required
+                      value={firstName}
+                    ></input>
+                  </label>
+                </StyledFlexColumn>
+                <StyledFlexColumn>
+                  <label>
+                    <p>Last name:</p>
+                    <input
+                      type="text"
+                      onChange={(e) => setLastName(e.target.value)}
+                      required
+                      value={lastName}
+                    ></input>
+                  </label>
+                </StyledFlexColumn>
+              </StyledFlexRow>
+              <StyledFlexColumn>
+                <label>
+                  <p>Email:</p>
+                  <input
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    value={email}
+                    style={{ width: '100%' }}
+                  ></input>
+                </label>
+              </StyledFlexColumn>
+
+              <StyledFlexRow padding="0.5em 0" justify="space-between">
+                <StyledFlexColumn>
+                  <label>
+                    <p>Cell number:</p>
+                    <input
+                      type="text"
+                      onChange={(e) => setCellNumber(e.target.value)}
+                      required
+                      value={cellNumber}
+                    ></input>
+                  </label>
+                </StyledFlexColumn>
+                <StyledFlexColumn>
+                  <label>
+                    <p>Zip code:</p>
+                    <input
+                      type="text"
+                      onChange={(e) => setZipCode(e.target.value)}
+                      required
+                      value={zipCode}
+                    ></input>
+                  </label>
+                </StyledFlexColumn>
+              </StyledFlexRow>
+
+              <StyledFlexColumn
+                style={{
+                  borderTop: '1px solid grey',
+                  margin: '0.5em ',
+                  padding: '0.5em',
+                }}
+              >
+                <StyledFlexRow justify="space-around">
+                  <div
+                    style={{
+                      border: '1px solid black',
+
+                      padding: '0.5em',
+                    }}
+                  >
+                    {' '}
+                    Dummy box
+                  </div>
+                  <div
+                    style={{
+                      border: '1px solid black',
+
+                      padding: '0.5em',
+                    }}
+                  >
+                    Dummy box
+                  </div>
+                  <div
+                    style={{
+                      border: '1px solid black',
+
+                      padding: '0.5em',
+                    }}
+                  >
+                    Dummy box
+                  </div>
+                </StyledFlexRow>
+                <label>
+                  <StyledFlexRow justify="space-between" margin="1em auto">
+                    <p>
+                      <input
+                        required
+                        type="checkbox"
+                        style={{ margin: '0 1em', padding: 0 }}
+                      ></input>
+                    </p>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Doloribus unde ullam ratione odit, tempora, in delectus
+                      quae dicta, quaerat dolor illum impedit natus inventore
+                      vero. Incidunt ipsum quidem repudiandae iste?
+                    </p>
+                  </StyledFlexRow>
+                </label>
+                <label>
+                  <StyledFlexRow justify="space-between">
+                    <p>
+                      <input
+                        required
+                        type="checkbox"
+                        style={{ margin: '0 1em', padding: 0 }}
+                      ></input>
+                    </p>
+                    <p>
+                      <Link to="/">
+                        I have read and accept the Altitude Appleton&quote;s
+                        terms and conditions
+                      </Link>
+                    </p>
+                  </StyledFlexRow>
+                </label>
+              </StyledFlexColumn>
+
+              <StyledFlexRow justify="space-between">
+                <StyledButton
+                  onClick={() => {
+                    setStep3(false)
+                    setStep4(false)
+                  }}
+                  bg="#d9d9d9"
+                  color="#000"
+                  width="40%"
+                >
+                  Back
+                </StyledButton>
+
+                <StyledButton
+                  width="50%"
+                  color="#fff"
+                  onClick={() => {
+                    setStep4(true)
+                    setStep3(false)
+                  }}
+                  type="submit"
+                >
+                  Continue
+                </StyledButton>
+              </StyledFlexRow>
+            </StyledFlexColumn>
+          </Form>
+        </StyledModal>
+      )}
+      {step4 && <PaymentDetails />}
+
+      <Recipe />
+    </StyledFlexRow>
+  )
+}
---
+++ src/components/Customer/modal-components/PaymentDetails.js
@@ -0,0 +1,196 @@
+import { useState } from 'react'
+//style
+import { StyledModal } from '../styled/Modal.styles'
+import { StyledRange } from '../styled/Range.styles'
+import { StyledFlexColumn } from '../styled/FlexColumn.styles'
+import { StyledFlexRow } from '../styled/FlexRow.styles'
+import { StyledButton } from '../styled/Button.styles'
+
+//assets
+import {
+  FaArrowLeft,
+  FaCcDinersClub,
+  FaCcJcb,
+  FaCcMastercard,
+  FaCreditCard,
+  FaGooglePay,
+  FaLock,
+} from 'react-icons/fa'
+import { SiAmericanexpress, SiVisa } from 'react-icons/si'
+
+//components
+
+import Recipe from '../modals/Recipe'
+
+import { Form } from 'reactstrap'
+import CustomerDetails from './CustomerDetail'
+
+//components showing add-ons options
+export default function PaymentDetails() {
+  const [step3, setStep3] = useState(false)
+  const [step4, setStep4] = useState(true)
+  return (
+    <StyledFlexRow>
+      {step4 && (
+        <StyledModal top={0} left="0" height="100%" width="35%">
+          <div>
+            <hr />
+            <StyledRange bg="#d9d9d9" width="35%">
+              <StyledRange bg="#35bd21" width="35%" />
+            </StyledRange>
+
+            <StyledFlexColumn
+              style={{
+                marginTop: '4em',
+              }}
+            >
+              <div>
+                <FaArrowLeft /> <span>Step 4 of 4</span>
+              </div>
+
+              <h3>Select payment details</h3>
+            </StyledFlexColumn>
+          </div>
+          <Form className="my-2">
+            <StyledFlexColumn>
+              <label>
+                <StyledFlexRow justify="space-between">
+                  <StyledFlexRow justify="left">
+                    <input
+                      type="radio"
+                      style={{ margin: '0 1em' }}
+                      name="paymentMethod"
+                    />
+                    <FaCreditCard
+                      style={{
+                        alignContent: 'left',
+                        margin: '0 ',
+                        alignSelf: 'center',
+                      }}
+                    />
+                    <p
+                      style={{
+                        alignContent: 'left',
+                        alignSelf: 'center',
+                        margin: '0 0.3em',
+                      }}
+                    >
+                      Debit / Credit card
+                    </p>
+                  </StyledFlexRow>
+
+                  <StyledFlexRow
+                    align="right"
+                    justify="space-between"
+                    width="100%"
+                  >
+                    <SiVisa
+                      style={{
+                        color: 'blue',
+                        background: 'white',
+                        margin: '0 0.2em',
+                      }}
+                    />
+                    <FaCcMastercard
+                      style={{
+                        color: 'blue',
+                        background: 'white',
+                        margin: '0 0.2em',
+                      }}
+                    />
+                    <SiAmericanexpress
+                      style={{
+                        color: 'blue',
+                        background: 'white',
+                        margin: '0 0.2em',
+                      }}
+                    />
+                    <FaCcJcb
+                      style={{
+                        color: 'blue',
+                        background: 'white',
+                        margin: '0 0.2em',
+                      }}
+                    />
+                    <FaCcDinersClub
+                      style={{
+                        color: 'blue',
+                        background: 'white',
+                        margin: '0 0.2em',
+                      }}
+                    />
+                  </StyledFlexRow>
+                </StyledFlexRow>
+              </label>
+              <label>
+                <p>Card number</p>
+                <input
+                  type="number"
+                  style={{ width: '100%' }}
+                  placeholder={`1234 5678 9012 3456 `}
+                />
+              </label>
+              <StyledFlexRow justify="space-between">
+                <label>
+                  <p>Expirity date</p>
+                  <input type="number" placeholder="MM/YY" />
+                </label>
+                <label>
+                  <p>CVC / CVV</p>
+                  <input type="number" placeholder="3 digits" />
+                </label>
+              </StyledFlexRow>
+              <StyledButton width="100%">
+                <FaLock></FaLock> Pay $82.29
+              </StyledButton>
+            </StyledFlexColumn>
+            <StyledFlexRow justify="left">
+              <input
+                type="radio"
+                style={{ margin: '0 1em' }}
+                name="paymentMethod"
+              />
+              <FaGooglePay
+                style={{
+                  alignContent: 'left',
+                  margin: '0 ',
+                  width: '1.5em',
+                  alignSelf: 'center',
+                  fontSize: '2em',
+                  border: '1px solid black',
+                  borderRadius: '5px',
+                }}
+              />
+              <p
+                style={{
+                  alignContent: 'left',
+                  alignSelf: 'center',
+                  margin: '0 0.3em',
+                }}
+              >
+                Google Pay
+              </p>
+            </StyledFlexRow>
+          </Form>
+          <hr />
+          <StyledFlexRow justify="space-around">
+            <StyledButton
+              onClick={() => {
+                setStep3(true)
+                setStep4(false)
+              }}
+              bg="#d9d9d9"
+              color="#000"
+              width="100%"
+            >
+              Back
+            </StyledButton>
+          </StyledFlexRow>
+        </StyledModal>
+      )}
+      {step3 && <CustomerDetails />}
+
+      <Recipe />
+    </StyledFlexRow>
+  )
+}
