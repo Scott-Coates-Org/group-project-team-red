674c70c41998be6e0097f9889734ed7c490c832d
pablo-maff <pablo.maff.076@gmail.com>
2022-07-07 21:16:41 +0200

    Header now is Home. Removed files from Customer UI 25% and some template files

--- src/components/app.js
+++ src/components/app.js
@@ -10,11 +10,10 @@ import { Route, Router, Switch } from 'react-router-dom'
 import store from 'redux/store'
 import { getData, getDataSuccess } from 'redux/user'
 import ErrorBoundary from 'components/error-boundary'
-import Home from './home'
-// import Header from './Customer/Header'
 import Dashboard from './admin-ui/dashboard/Dashboard'
 import CustomerWaiver from './Customer/waiver/CustomerWaiver'
 import ThankYou from './Customer/thank-you/ThankYou'
+import Home from './Customer/Home'
 
 // DO NOT import BrowserRouter (as per tutorial). that caused router to not actually do anything.
 // see here: https://stackoverflow.com/questions/63554233/react-router-v5-history-push-changes-the-address-bar-but-does-not-change-the
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
 import { reducer as user } from './user'
-import { reducer as widget } from './widget'
 import { reducer as product } from './product'
 import { reducer as room } from './room'
 import { reducer as addOn } from './addOn'
 
 const reducer = combineReducers({
   user,
-  widget,
   product,
   room,
   addOn,
